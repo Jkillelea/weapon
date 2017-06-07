2e466124e2be9a91b7d554ba4086ddd8167fe73d
Jacob Killelea <Jkillelea@protonmail.ch>
2017-06-07 12:27:15 -0700

    formatting

--- src/frame.rs
+++ src/frame.rs
@@ -7,8 +7,8 @@ pub type Row   = Vec<char>;
 pub type Frame = Vec<Row>;
 
 pub fn file_to_frame(filename: &str) -> io::Result<Frame> {
-    let     file  = File::open(filename)?;
-    let mut file  = BufReader::new(file);
+    let     file = File::open(filename)?;
+    let mut file = BufReader::new(file);
 
     let mut frame = Vec::new();
     let mut text  = String::new();         // allocate here instead of inside loop
@@ -16,10 +16,10 @@ pub fn file_to_frame(filename: &str) -> io::Result<Frame> {
     while file.read_line(&mut text)? > 0 { // read all bytes, append to string
         let mut row = Vec::new();          // have to allocate here since we're transferring ownership
         for c in text.chars() {
-            &row.push(c);
+            row.push(c);
         }
         frame.push(row);
-        text.clear(); // remember to clear the buffer after each!
+        text.clear(); // remember to clear the buffer after each! (doesn't deallocate)
     }
     Ok(frame)
 }

abcd8b9478d208fdf1095d359cac3cb7a5a41a9a
Jacob Killelea <jkillelea@protonmail.ch>
2017-06-07 18:31:56 -0700

    pre-reset

--- src/frame.rs
+++ src/frame.rs
@@ -7,19 +7,20 @@ pub type Row   = Vec<char>;
 pub type Frame = Vec<Row>;
 
 pub fn file_to_frame(filename: &str) -> io::Result<Frame> {
-    let     file  = File::open(filename)?;
-    let mut file  = BufReader::new(file);
+    let     file = File::open(filename)?;
+    let mut file = BufReader::new(file);
 
     let mut frame = Vec::new();
-    let mut text  = String::new();         // allocate here instead of inside loop
+    let mut buff  = String::new();
 
-    while file.read_line(&mut text)? > 0 { // read all bytes, append to string
-        let mut row = Vec::new();          // have to allocate here since we're transferring ownership
-        for c in text.chars() {
+    // read all bytes, append to string
+    while file.read_line(&mut buff)? > 0 {
+        let mut row = Vec::new();
+        for c in buff.chars() {
             &row.push(c);
         }
         frame.push(row);
-        text.clear(); // remember to clear the buffer after each!
+        buff.clear(); // remember to clear the buffer after each!
     }
     Ok(frame)
 }
--- src/render.rs
+++ src/render.rs
@@ -8,15 +8,14 @@ pub fn draw<W: Write>(interface: &mut W, frame: frame::Frame) -> io::Result<()> 
     for (y, row) in frame.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
            let xpos = (x + 1) as u16;
-           let ypos = (y + 1) as u16; 
+           let ypos = (y + 1) as u16;
            write!(
-               interface, 
-               "{}{}", 
-               cursor::Goto(xpos, ypos), 
+               interface,
+               "{}{}",
+               cursor::Goto(xpos, ypos),
                c
            )?;
         }
     }
     Ok(())
 }
-

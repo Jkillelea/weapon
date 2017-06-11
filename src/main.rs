c5de3b955e7565b031c2944d5e97762804f33ba0
Jacob Killelea <jkillelea@protonmail.ch>
2017-06-10 23:29:56 -0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 extern crate termion;
+// output
 use termion::raw::IntoRawMode;
 use termion::{cursor, clear};
+// input
+use termion::event::Key;
+use termion::input::TermRead;
 
 use std::env;
 use std::io;
 use std::io::prelude::*;
 
-use std::thread::sleep;
-use std::time::Duration;
-
 mod frame;
 mod render;
+
+/* Things that are working: Raw mode, dispalying text
+ * Things that need to work: Cursor movement, changing text
+ * IDK when this will have work done on it
+ */
 
 #[allow(dead_code, unused_variables)]
 fn main() {
@@ -21,12 +27,23 @@ fn main() {
     let     stdout = io::stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
 
+    let stdin = io::stdin();
+
     write!(stdout, "{}", clear::All).unwrap();
     write!(stdout, "{}", cursor::Goto(1, 1)).unwrap();
 
     render::draw(&mut stdout, frame).unwrap();
 
-    sleep(Duration::from_secs(5));
+    for k in stdin.keys() {
+        let k = k.unwrap();
+        println!("{:?}\r", k);
+        match k {
+            Key::Char('q') |
+            Key::Ctrl('c') => {break},
+            _ => {},
+
+        }
+    }
 
     stdout.flush().unwrap();
 }

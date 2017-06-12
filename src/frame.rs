1ea16eb9522c30ceb4a878e58e9db9ff614a2e0d
Jacob Killelea <jkillelea@protonmail.ch>
2017-06-11 21:15:00 -0700

    quick fix, post merge

--- src/frame.rs
+++ src/frame.rs
@@ -13,13 +13,13 @@ pub fn file_to_frame(filename: &str) -> io::Result<Frame> {
     let mut frame = Vec::new();
     let mut buff  = String::new();
 
-    while file.read_line(&mut text)? > 0 { // read all bytes, append to string
+    while file.read_line(&mut buff)? > 0 { // read all bytes, append to string
         let mut row = Vec::new();          // have to allocate here since we're transferring ownership
-        for c in text.chars() {
+        for c in buff.chars() {
             row.push(c);
         }
         frame.push(row);
-        text.clear(); // remember to clear the buffer after each! (doesn't deallocate)
+        buff.clear(); // remember to clear the buffer after each! (doesn't deallocate)
     }
     Ok(frame)
 }

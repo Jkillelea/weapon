04bffd3d771dfb8e131de0285c0e1c717ec955a2
Jacob Killelea <jkillelea@protonmail.ch>
2017-06-11 22:30:20 -0700

    change Vec::new() to Frame::new() or Row::new()

--- src/frame.rs
+++ src/frame.rs
@@ -10,11 +10,11 @@ pub fn file_to_frame(filename: &str) -> io::Result<Frame> {
     let     file = File::open(filename)?;
     let mut file = BufReader::new(file);
 
-    let mut frame = Vec::new();
+    let mut frame = Frame::new();
     let mut buff  = String::new();
 
     while file.read_line(&mut buff)? > 0 { // read all bytes, append to string
-        let mut row = Vec::new();          // have to allocate here since we're transferring ownership
+        let mut row = Row::new();          // have to allocate here since we're transferring ownership
         for c in buff.chars() {
             row.push(c);
         }
